8d533ae72b6b29e78a675b4300e90252941b8002
hhjjfly-alt <hhjjfly@gmail.com>
2025-08-27 10:16:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -13,7 +13,7 @@ echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-inf
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
-
+echo 'src-git smartdns_luci https://github.com/pymumu/luci-app-smartdns;lede' >>feeds.conf.default
 # iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
 
