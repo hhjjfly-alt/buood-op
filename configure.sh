d244a7374a56f4cd249d595299adb52ebb4b7a14
hhjjfly-alt <hhjjfly@gmail.com>
2025-08-27 10:14:20 +0800

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Modify default system settings
+
+# 暂时回滚
+sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
+
+# 修改默认IP为192.168.10.1
+sed -i 's/192.168.1.1/10.0.0.8/g' package/base-files/files/bin/config_generate 
+
+# luci-theme-infinityfreedom
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+
+# passwall
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+
+# iStore
+echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# 替换默认主题
+rm -rf package/lean/luci-theme-argon 
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
