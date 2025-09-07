45fc95ae6bb9afa55d4e4c68d76463b6e748e4fd
hhjjfly-alt <hhjjfly@gmail.com>
2025-09-07 19:06:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-#
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # MIT License
-# 描述: OpenWrt DIY script part 2 (After Update feeds)
+# 描述: OpenWrt DIY script part 2（After Update feeds）
 
-# 1. 修改默认 IP / 主机名 / 固件名 / 版本号
+########### 万能函数 ###########
+clone_or_pull() {
+  local repo=$1 dir=$2
+  if [[ -d "$dir/.git" ]]; then
+    git -C "$dir" fetch --depth 1 && git -C "$dir" reset --hard origin/HEAD
+  else
+    git clone --depth 1 "$repo" "$dir"
+  fi
+}
+
+########### 1. 个性化设置 ###########
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 sed -i 's/IMG_PREFIX:=.*/IMG_PREFIX:=full-$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 pushd package/lean/default-settings/files
@@ -13,19 +22,16 @@ orig_version="$(grep DISTRIB_REVISION zzz-default-settings | awk -F"'" '{print $
 sed -i "s/${orig_version}/${orig_version} ($(date +%Y-%m-%d))/g" zzz-default-settings
 popd
 
-# 2. 强制重新下载 PassWall 组件源码（确保每次都用上游最新）
-rm -rf feeds/passwall_packages/* feeds/passwall_luci/*
-# 强制重新下载 ChinaDNS-NG 源码（保证每次编译都是上游 HEAD）
-rm -rf feeds/chinadns_ng/*
-# 3. SmartDNS 版本 bump（可选）
+########### 2. 强制刷新官方组件源码（永远最新） ###########
+rm -rf feeds/chinadns_ng/* feeds/passwall_packages/* feeds/passwall_luci/*
+
+########### 3. SmartDNS 版本 bump（可选） ###########
 sed -i 's/1.2024.45/1.2024.46/g; s/9ee27e7ba2d9789b7e007410e76c06a957f85e98/b525170bfd627607ee5ac81f97ae0f1f4f087d6b/g; /^PKG_MIRROR_HASH/s/^/#/' \
        feeds/packages/net/smartdns/Makefile
 
-# 4. 额外插件（按需保留）
+########### 4. 额外插件（按需） ###########
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
-pushd package/lean
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
-popd
+pushd package/lean && git clone --depth=1 https://github.com/lisaac/luci-app-dockerman && popd
 
-# 5. 连接数优化
+########### 5. 系统调优 ###########
 echo 'net.netfilter.nf_conntrack_max=165535' >> package/base-files/files/etc/sysctl.conf
