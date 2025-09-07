ef90cd7d6008cccf74841b6bc19c6fc29cc97da0
hhjjfly-alt <hhjjfly@gmail.com>
2025-09-07 16:01:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -63,5 +63,5 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 # ---------- iStore 官方源 ----------
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
+# ./scripts/feeds update istore
+# ./scripts/feeds install -d y -p istore luci-app-store
