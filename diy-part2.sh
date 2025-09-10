1b467d731076166a3ba0b6173e9c56be8e098f80
hhjjfly-alt <hhjjfly@gmail.com>
2025-09-10 19:20:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ clone_or_pull() {
 
 ########### 0. 若 istore 已存在则跳过（避免重复） ###########
 grep -q '^src-git istore' feeds.conf.default || {
-  echo 'src-git istore https://github.com/linkease/istore ;main' >> feeds.conf.default
+  echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
   ./scripts/feeds update istore
   ./scripts/feeds install -d y -p istore luci-app-store
 }
