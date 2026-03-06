1c9eecc4facb2eac5b443751b4aa9c657fa2c589
hhjjfly-alt <hhjjfly@gmail.com>
2026-03-06 17:04:32 +0800

    Add files via upload

---
+++ diyyb-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+add_feed() {
+local name=$1 url=$2
+if ! grep -q "^src-git $name " feeds.conf.default; then
+echo "src-git $name $url" >> feeds.conf.default
+fi
+}
+add_feed istore 'https://github.com/linkease/istore;main'
+add_feed istore_packages 'https://github.com/linkease/istore-packages;main'
+add_feed smartdns_luci 'https://github.com/pymumu/luci-app-smartdns;lede'
+add_feed passwall_packages 'https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
+add_feed passwall_luci 'https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
+add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;main'
+add_feed ddnsgo 'https://github.com/sirpdboy/luci-app-ddns-go'
+sed -i 's/KERNEL_PATCHVER:=./KERNEL_PATCHVER:=6.12/' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=./KERNEL_TESTING_PATCHVER:=6.12/' target/linux/x86/Makefile
+mkdir -p package/passwall-force-latest
+cat >> package/passwall-force-latest/Makefile <<'EOF'
+PKG_SOURCE_VERSION:=$(shell git ls-remote https://github.com/Openwrt-Passwall/openwrt-passwall-packages HEAD | awk '{print $$  1}')
+EOF
