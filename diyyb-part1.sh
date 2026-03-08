29959f5b4642b9195fdc609b6a55432e6a95245a
hhjjfly-alt <hhjjfly@gmail.com>
2026-03-08 19:37:14 +0800

    Update diyyb-part1.sh

--- diyyb-part1.sh
+++ diyyb-part1.sh
@@ -5,16 +5,20 @@ if ! grep -q "^src-git $name " feeds.conf.default; then
 echo "src-git $name $url" >> feeds.conf.default
 fi
 }
+
 add_feed istore 'https://github.com/linkease/istore;main'
 add_feed istore_packages 'https://github.com/linkease/istore-packages;main'
 add_feed smartdns_luci 'https://github.com/pymumu/luci-app-smartdns;lede'
 add_feed passwall_packages 'https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
 add_feed passwall_luci 'https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
-add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;main'
+add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;master' # <-- 修复：正确的分支是 master
 add_feed ddnsgo 'https://github.com/sirpdboy/luci-app-ddns-go'
+
 sed -i 's/KERNEL_PATCHVER:=./KERNEL_PATCHVER:=6.12/' target/linux/x86/Makefile
 sed -i 's/KERNEL_TESTING_PATCHVER:=./KERNEL_TESTING_PATCHVER:=6.12/' target/linux/x86/Makefile
+
 mkdir -p package/passwall-force-latest
 cat >> package/passwall-force-latest/Makefile <<'EOF'
-PKG_SOURCE_VERSION:=$(shell git ls-remote https://github.com/Openwrt-Passwall/openwrt-passwall-packages HEAD | awk '{print $$  1}')
+# 修复了 awk 后面多余空格导致的语法错误
+PKG_SOURCE_VERSION:=$(shell git ls-remote https://github.com/Openwrt-Passwall/openwrt-passwall-packages HEAD | awk '{print $$1}')
 EOF
