652e10b90a6c94a58ff35e121afe963a94c6498a
hhjjfly-alt <hhjjfly@gmail.com>
2026-03-11 09:41:47 +0800

    Update diyyb-part1.sh

--- diyyb-part1.sh
+++ diyyb-part1.sh
@@ -9,9 +9,3 @@ fi
 add_feed istore 'https://github.com/linkease/istore;main'
 add_feed istore_packages 'https://github.com/linkease/istore-packages;main'
 add_feed smartdns_luci 'https://github.com/pymumu/luci-app-smartdns;lede'
-add_feed passwall_packages 'https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
-add_feed passwall_luci 'https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
-add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;master'
-add_feed ddnsgo 'https://github.com/sirpdboy/luci-app-ddns-go'
-
-# 注意：已移除会导致源码扫描崩溃的残缺 Makefile 生成脚本
