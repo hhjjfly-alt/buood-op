8d632dc835df26613b2ee0ce5e35a341686fd9f0
hhjjfly-alt <hhjjfly@gmail.com>
2026-03-11 09:20:43 +0800

    Update diyyb-part1.sh

--- diyyb-part1.sh
+++ diyyb-part1.sh
@@ -11,14 +11,7 @@ add_feed istore_packages 'https://github.com/linkease/istore-packages;main'
 add_feed smartdns_luci 'https://github.com/pymumu/luci-app-smartdns;lede'
 add_feed passwall_packages 'https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
 add_feed passwall_luci 'https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
-add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;master' # <-- 修复：正确的分支是 master
+add_feed chinadns_ng 'https://github.com/zfl9/chinadns-ng;master'
 add_feed ddnsgo 'https://github.com/sirpdboy/luci-app-ddns-go'
 
-sed -i 's/KERNEL_PATCHVER:=./KERNEL_PATCHVER:=6.12/' target/linux/x86/Makefile
-sed -i 's/KERNEL_TESTING_PATCHVER:=./KERNEL_TESTING_PATCHVER:=6.12/' target/linux/x86/Makefile
-
-mkdir -p package/passwall-force-latest
-cat >> package/passwall-force-latest/Makefile <<'EOF'
-# 修复了 awk 后面多余空格导致的语法错误
-PKG_SOURCE_VERSION:=$(shell git ls-remote https://github.com/Openwrt-Passwall/openwrt-passwall-packages HEAD | awk '{print $$1}')
-EOF
+# 注意：已移除会导致源码扫描崩溃的残缺 Makefile 生成脚本
